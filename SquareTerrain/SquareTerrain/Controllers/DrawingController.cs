f9a7ec9506aebead7747dc9fc376655a4bc2a12b
Mateusz Kordula <mkordula93@gmail.com>
2015-03-04 22:07:39 +0100

    init

---
+++ SquareTerrain/SquareTerrain/Controllers/DrawingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace SquareTerrain.Controllers
+{
+    public class DrawingController
+    {
+        private readonly RenderWindow _mainWindow;
+
+        public DrawingController(RenderWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        public void Draw()
+        {
+        }
+    }
+}
---
+++ SquareTerrain/SquareTerrain/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+using SquareTerrain.Controllers;
+
+namespace SquareTerrain
+{
+    class Program
+    {
+        private static RenderWindow _mainWindow;
+        private static DrawingController _drawingManager;
+
+        static void Main(string[] args)
+        {
+            _mainWindow = new RenderWindow(new VideoMode(800,600), "Square terrain generator");
+            _mainWindow.Closed += mainWindow_Closed;
+
+            _drawingManager = new DrawingController(_mainWindow);
+
+            while (_mainWindow.IsOpen())
+            {
+                _mainWindow.DispatchEvents();
+
+                _mainWindow.Clear(Color.Black);
+                _drawingManager.Draw();
+                _mainWindow.Display();
+            }
+        }
+
+        static void mainWindow_Closed(object sender, EventArgs e)
+        {
+            _mainWindow.Close();
+        }
+    }
+}
